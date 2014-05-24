61d7156903bb7167224c2495ad8d0193a17bef07
Brandon Valosek <bvalosek@gmail.com>
2014-05-23 21:50:30 -0500

    Mapping single objects nicely

--- test/mapped-repo.js
+++ test/mapped-repo.js
@@ -1,12 +1,12 @@
 var test = require('tape');
 
 var MappedRepo = require('../lib/MappedRepo.js');
+var Promise    = require('es6-promise').Promise;
 
 test('Missing implementations', function(t) {
   t.plan(7);
 
   function T() { }
-
   var m = new MappedRepo(T);
 
   m.get().catch(function() { t.pass(); });
@@ -16,5 +16,27 @@ test('Missing implementations', function(t) {
   m.update().catch(function() { t.pass(); });
   m.fetch().catch(function() { t.pass(); });
   m.query().catch(function() { t.pass(); });
+
+});
+
+test('Map output to single', function(t) {
+  t.plan(3);
+
+  function T() { }
+  var m = new MappedRepo(T);
+  var RET;
+  m._get = function() { return RET; };
+
+  RET = 123;
+  m.get()
+    .then(function(x) { t.strictEqual(x, 123); })
+
+    .then(function() { RET = [123]; })
+    .then(function() { return m.get(); })
+    .then(function(x) { t.strictEqual(x, 123); })
+
+    .then(function() { RET = Promise.resolve(123); })
+    .then(function() { return m.get(); })
+    .then(function(x) { t.strictEqual(x, 123); });
 
 });
