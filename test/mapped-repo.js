7ddaa11ff768aa5401714c88bf7114480d910adc
Brandon Valosek <bvalosek@gmail.com>
2014-05-23 21:52:10 -0500

    one more

--- test/mapped-repo.js
+++ test/mapped-repo.js
@@ -20,22 +20,30 @@ test('Missing implementations', function(t) {
 });
 
 test('Map output to single', function(t) {
-  t.plan(3);
+  t.plan(4);
 
   function T() { }
   var m = new MappedRepo(T);
   var RET;
   m._get = function() { return RET; };
 
+  // plain value
   RET = 123;
   m.get()
     .then(function(x) { t.strictEqual(x, 123); })
 
+    // array-ed value
     .then(function() { RET = [123]; })
     .then(function() { return m.get(); })
     .then(function(x) { t.strictEqual(x, 123); })
 
+    // Promise for a val
     .then(function() { RET = Promise.resolve(123); })
+    .then(function() { return m.get(); })
+    .then(function(x) { t.strictEqual(x, 123); })
+
+    // Promise for an array-ed val
+    .then(function() { RET = Promise.resolve([123]); })
     .then(function() { return m.get(); })
     .then(function(x) { t.strictEqual(x, 123); });
 
