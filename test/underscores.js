7b5b0c1dc0c10455ac74323c17a3cf7f3918e616
Brandon Valosek <bvalosek@gmail.com>
2014-05-27 00:13:03 -0500

    actual tests

---
+++ test/underscores.js
@@ -0,0 +1,32 @@
+var test      = require('tape');
+var transform = require('../util/removeUnderscores.js');
+
+
+test('output', function(t) {
+  t.plan(2);
+  var f = transform();
+  var o;
+
+  o = { a_b_c: 1 };
+  f(null, o);
+  t.deepEqual(o, { aBC: 1 });
+
+  o = { some_thing_nice: 1 };
+  f(null, o);
+  t.deepEqual(o, { someThingNice: 1 });
+
+});
+
+test('input', function(t) {
+  t.plan(2);
+  var f = transform();
+  var o;
+
+  o = { aBC: 1 };
+  f(o, null);
+  t.deepEqual(o, {a_b_c: 1});
+
+  o = { someThingNice: 1 };
+  f(o, null);
+  t.deepEqual(o, {some_thing_nice: 1});
+});
