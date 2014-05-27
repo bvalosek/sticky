2c4375f7f2663eb0e08515cc0cdc8c899235ba5b
Brandon Valosek <bvalosek@gmail.com>
2014-05-27 00:09:31 -0500

    tests for underscore remove

--- util/removeUnderscores.js
+++ util/removeUnderscores.js
@@ -1,43 +1,38 @@
 module.exports = removeUnderscores;
 
-function unfuck(s)
+/**
+ * A sticky transform that will change vars_like_this into varsLikeThis.
+ */
+function removeUnderscores()
+{
+  return function(input, output)
+  {
+    var target = output || input;
+    var f = output ? remove : add;
+
+    for (var key in target) {
+      var value = target[key];
+      var key_ = f(key);
+      if (key_ === key) continue;
+      delete target[key];
+      target[key_] = value;
+    }
+
+  };
+}
+
+function remove(s)
 {
   return s.replace(/_([a-z])/g, function(m, w) {
     return w.toUpperCase();
   });
 }
 
-function fuck(s)
+function add(s)
 {
-  return s.replace(/([a-z])([A-Z])/g, function(m, a, b) {
-    return a + '_' + b;
+  return s.replace(/([a-z]*)([A-Z])/g, function(m, a, b) {
+    return a + '_' + b.toLowerCase();
   });
 }
 
 
-function removeUnderscores()
-{
-  return function(input, output, instance)
-  {
-    var key, key_, value;
-
-    // Change output to be non camelized
-    if (output) {
-      for (key in output) {
-        value = output[key];
-        key_ = unfuck(key);
-        if (key_ === key) continue;
-        delete output[key];
-        output[key_] = value;
-      }
-    } else {
-      for (key in input) {
-        value = input[key];
-        key_ = fuck(key);
-        if (key_ === key) continue;
-        delete input[key];
-        input[key_] = value;
-      }
-    }
-  };
-}
